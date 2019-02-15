5d1489443dae1bddd287115bb568d8c725b9148d
brenden <brendend13@gmail.com>
2019-02-15 11:48:27 -0600

    Began work on cellGrid

--- src/layout/cellGrid.jsx
+++ src/layout/cellGrid.jsx
@@ -21,16 +21,18 @@ class CellGrid extends Component {
         initCellGrid();
     }
 
+    // TODO: Rework Cell class so that it gets its info from here. 
+
     /*
         This function maps Cell objects to the 2-D array, cells, in the object state.
         This is strictly a logical mapping. Nothing is drawn at this point.
     */
     initCellGrid(){
         let idx = this.state.startIndex;
-        // Run this for each column to make (x-value)
-        for (let x = 0; x < this.state.columns; x++){
-            // Run this for each row to make (y-value)
-            for (let y = 0; y < this.state.rows; y++){
+        // Run this for each row to make (x-value)
+        for (let x = 0; x < this.state.rows; x++){
+            // Run this for each column to make (y-value)
+            for (let y = 0; y < this.state.columns; y++){
                 this.state.cells[x][y] = new Cell({
                     index: idx
                 });
@@ -47,17 +49,29 @@ class CellGrid extends Component {
         );
     }
 
+    renderCellRow(size) {
+        /* 
+            A cell row will will have the same number of columns as there are in the
+            state, so we can use that value to determine how many cells to render.
+        */
+        for (let col = 0; col < this.state.columns; col++){
+            
+        }
+    }
+
     render() {
         // Display the cells according to their row and column info
 
         /*
-            TODO: Figure out how to properly iterate through the cell array, and
-            render them to the page.
+            Cells should be rendered row by row.
         */
+        let row = 0;
+
         return (
             <div>
                 <div className="cell-row">
                     {
+
                     }
                 </div>
             </div>
