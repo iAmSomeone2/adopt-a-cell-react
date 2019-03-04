9017d77b1e6ab3efc8e76d28540c36c674444d62
Brenden Davidson <david679@umn.edu>
2019-03-03 19:02:19 -0600

    Initialized DetailCell object.

---
+++ src/layout/detailCell.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "./css/cell.css"
+import UnclaimedCell from './assets/solar-cell_unclaimed.svg';
+import ClaimedCell from './assets/solar-cell_claimed.svg';
+
+const unclaimed_cell_desc = "Unclaimed cell #";
+const claimed_cell_desc = "Cell adopted by: ";
+
+class DetailCell extends Component {
+    
+}
+
+export default DetailCell;
