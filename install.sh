29e30d398b396fc6a6364b8d46f342556cce3acb
Brenden Davidson <brendend13@gmail.com>
2019-04-10 15:27:39 -0500

    Fixed install script.

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo Running build...
+npm run build
+echo Copying build files to prod...
+cp -r build/* /var/www/cell.bdavidson.dev/html/
+echo Restarting nginx...
+systemctl restart nginx
+echo DONE!\n
--- src/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-npm run build
-cp build/* /var/www/cell.bdavidson.dev/html/
-systemctl restart nginx
